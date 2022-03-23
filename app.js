15fdf0e5a4937cd51b2ce9865967c8ca233c3252
sajjadKh <sajad.khosravi2013@gmail.com>
2022-03-24 02:20:54 +0430

    add space in app.js

--- app.js
+++ app.js
@@ -1,92 +1,93 @@
-const express=require("express");
-const path=require("path");
-const app=express();
-const hbs=require("hbs");
-const morgan =require("morgan");
-const weather=require("./weather");
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+const morgan = require("morgan");
+const weather = require("./weather");
 const async = require("hbs/lib/async");
-const port=3000;
+const port = 3000;
 
-const directoryPublicPath=path.join(__dirname,"/public");
-const directoryViewsPath=path.join(__dirname,"/templates");
-const directoryPartialsPath=path.join(directoryViewsPath,"/partials");
+
+const directoryPublicPath = path.join(__dirname, "/public");
+const directoryViewsPath = path.join(__dirname, "/templates");
+const directoryPartialsPath = path.join(directoryViewsPath, "/partials");
 app.use(express.static("public"));
 app.set('view engine', 'hbs');
-app.set('views',directoryViewsPath);
+app.set('views', directoryViewsPath);
 hbs.registerPartials(directoryPartialsPath);
 
 //middleware
 
-const accessWather=(req,res,next)=>{
-    if(req.params.city ==="paris") return res.send(403);
+const accessWather = (req, res, next) => {
+    if (req.params.city === "paris") return res.send(403);
     next();
-    
+
 }
 
-const logger=(req,res,next)=>{
-    if(req.originalUrl === "/favicon.ico") return next();
+const logger = (req, res, next) => {
+    if (req.originalUrl === "/favicon.ico") return next();
     console.log(req.originalUrl);
-    
+
     next();
 }
 
 app.use(morgan('dev'));
 
-app.get("/",(req,res)=>{
-    res.render("base",{
-        title:"Home",
-        h1Title:"Home Page",
-        content:"Welcome To Home Page."
+app.get("/", (req, res) => {
+    res.render("base", {
+        title: "Home",
+        h1Title: "Home Page",
+        content: "Welcome To Home Page."
     });
 
 });
-app.get("/about",(req,res)=>{
-    res.render("base",{
-        title:"About US",
-        h1Title:"About Us Page ",
-        content:"Welcome To About Us Page"
+app.get("/about", (req, res) => {
+    res.render("base", {
+        title: "About US",
+        h1Title: "About Us Page ",
+        content: "Welcome To About Us Page"
     });
 
 });
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
     res.send([{
-        name:"sajjad",
-        email:"Sajjad.khosravi2013@gmail.com"
-    },{
-        name:"Ali",
-        email:"ali@gmail.com"
+        name: "sajjad",
+        email: "Sajjad.khosravi2013@gmail.com"
+    }, {
+        name: "Ali",
+        email: "ali@gmail.com"
     }])
 
-  
+
 })
 
-app.get("/weather", async (req,res)=>{
-    const data= await weather.getWeather();
-    res.render("weather",{
-        title:"Get Weather",
-        h1Title:"Get Weather ",
-        content:` The ${data.location.name} temperature is  ${data.current.temperature}`
+app.get("/weather", async (req, res) => {
+    const data = await weather.getWeather();
+    res.render("weather", {
+        title: "Get Weather",
+        h1Title: "Get Weather ",
+        content: ` The ${data.location.name} temperature is  ${data.current.temperature}`
     });
 
 });
 
-app.get("/weather/:city",accessWather, async (req,res)=>{
-    const data= await weather.getWeather(req.params.city);
+app.get("/weather/:city", accessWather, async (req, res) => {
+    const data = await weather.getWeather(req.params.city);
     res.send(data);
-    
+
 
 });
 
-app.get('*',(req,res)=>{
-    res.statusCode=404;
-    res.render("404",{
-        title:"not found page",
-        h1Title:"404 : Not Found Page",
-        error:"oh Page Not Found ;("
+app.get('*', (req, res) => {
+    res.statusCode = 404;
+    res.render("404", {
+        title: "not found page",
+        h1Title: "404 : Not Found Page",
+        error: "oh Page Not Found ;("
     })
 
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 
 })
