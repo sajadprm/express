09abccde247d8fa419818470cd37cb16604b058d
sajjadKh <sajad.khosravi2013@gmail.com>
2022-04-28 18:03:19 +0430

    add access controling for update and delete

--- tools/ac.js
+++ tools/ac.js
@@ -8,4 +8,23 @@ accessController.checkAdminMidellware=(req,res,next)=>{
 
    return next();
 }
+
+
+accessController.checkAdminForDeleteUserOrUpdateUser=(req,res,next)=>{
+   if(req.user.role !=="admin" && req.params.id==="me")
+   {
+       res.locals.userId=req.user._id;
+        return next();
+      
+   }
+   if(req.user.role==="admin")
+   {
+      res.locals.userId=req.params.id;
+      return next();
+   }
+
+   res.sendStatus(403);
+
+}
+
 module.exports=accessController;
